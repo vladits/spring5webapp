834217f4b69581ed176b4b075cd810873b02a08b
Vladimir Tsiparsky <vladits@gmail.com>
2022-08-10 10:06:36 +0300

    Author and Book Controllers added

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by vladi on 10/08/2022
+ **/
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
